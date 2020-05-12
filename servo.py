53b0b517fd6ca5b6305f9f4a491dc87c54ae9405
bws428 <briandwendt@gmail.com>
2020-05-12 11:02:52 -0400

    Update naming convention

--- servo.py
+++ servo.py
@@ -4,10 +4,8 @@
 # Module:   servo.py
 # Created:  2 April 2008
 # Author:   Brian D. Wendt
-#   http://principialabs.com/
 # Version:  0.3
-# License:  GPLv3
-#   http://www.fsf.org/licensing/
+# License:  MIT
 '''
 Provides a serial connection abstraction layer
 for use with Arduino "MultipleSerialServoControl" sketch.
@@ -23,7 +21,7 @@ import serial
 #     usbport = '/dev/ttyUSB0'
 #   MacOSX example
 #     usbport = '/dev/tty.usbserial-FTALLOK2'
-usbport = '/dev/tty.usbserial-FTALLOK2'
+usbport = '/dev/ttyUSB0'
 
 # Set up serial baud rate
 ser = serial.Serial(usbport, 9600, timeout=1)
@@ -45,4 +43,4 @@ def move(servo, angle):
         ser.write(chr(servo))
         ser.write(chr(angle))
     else:
-        print "Servo angle must be an integer between 0 and 180.\n"
+        print("Servo angle must be an integer between 0 and 180.\n")
